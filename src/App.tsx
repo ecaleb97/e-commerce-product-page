ef577572f735b5727524b0333f5d6484bd3495ab
ecaleb97 <valley_rob.0c@icloud.com>
2023-12-29 16:38:12 +0100

    Delete unused image and icon files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Header } from "./components/Header"
 import { ImageSlider } from "./components/ImageSlider"
 import { ProductInfo } from "./components/ProductInfo"
-import { imgUrls } from "./lib/data"
 
 function App() {
+  const imgUrls = [
+    './assets/images/image-product-1.jpg',
+    './assets/images/image-product-2.jpg',
+    './assets/images/image-product-3.jpg',
+    './assets/images/image-product-4.jpg',
+  ]
+
   return (
     <>
       <Header />
