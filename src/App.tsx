ea73e7a06f532ecd59ab0226502641a283149e37
ecaleb97 <valley_rob.0c@icloud.com>
2023-12-29 16:45:38 +0100

    Update image URLs in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Header } from "./components/Header"
 import { ImageSlider } from "./components/ImageSlider"
 import { ProductInfo } from "./components/ProductInfo"
+import avatar from "./assets/images/image-avatar.png"
+import imageProduct1 from "./assets/images/image-product-1.jpg"
+import imageProduct2 from "./assets/images/image-product-2.jpg"
+import imageProduct3 from "./assets/images/image-product-3.jpg"
+import imageProduct4 from "./assets/images/image-product-4.jpg"
 
 function App() {
   const imgUrls = [
-    './assets/images/image-product-1.jpg',
-    './assets/images/image-product-2.jpg',
-    './assets/images/image-product-3.jpg',
-    './assets/images/image-product-4.jpg',
+    avatar,
+    imageProduct1,
+    imageProduct2,
+    imageProduct3,
+    imageProduct4,
   ]
 
   return (
